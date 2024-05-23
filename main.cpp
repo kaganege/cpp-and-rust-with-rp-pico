b6d1ac85a8a4583f7ccd26b4908ec166b37df100
kaganege <kaganegeozkan@gmail.com>
2024-05-23 23:27:35 +0300

    removed blank lines

--- main.cpp
+++ main.cpp
@@ -1,16 +1,13 @@
 #include "pico/stdlib.h"
-
 {% if board_type == "Pico W" %}
 #include "pico/cyw43_arch.h"
 {% endif %}
-
 #include "macros.h"
 #include "rusty.h"
 
 int main()
 {
   stdio_init_all();
-
   {% if board_type == "Pico" %}
   gpio_init(PICO_DEFAULT_LED_PIN);
   gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
@@ -22,7 +19,6 @@ int main()
     return -1;
   }
   {% endif %}
-  
 
   while (true)
   {
@@ -31,17 +27,13 @@ int main()
     {% else %}
     cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, true);
     {% endif %}
-    
     hello_from_rust();
     sleep_ms(500);
-
-
     {% if board_type == "Pico" %}
     gpio_put(PICO_DEFAULT_LED_PIN, false);
     {% else %}
     cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, false);
     {% endif %}
-
     hello_from_rust();
     sleep_ms(500);
   }
