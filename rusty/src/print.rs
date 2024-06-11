37dc74d46157ae1ff8f7ec08be50b1187681d18b
kaganege <kaganegeozkan@gmail.com>
2024-06-11 17:00:49 +0300

    improved print

--- rusty/src/print.rs
+++ rusty/src/print.rs
@@ -1,9 +1,26 @@
-::custom_print::define_macros!(#[macro_export] { print, println }, concat, |value: &str| {
-  unsafe { crate::printf(format!("{value}\0").as_ptr()) };
-});
+pub(crate) unsafe fn put_str_raw(value: &str) {
+  for char in value.chars() {
+    super::putchar_raw(char as _);
+  }
+}
 
-::custom_print::define_macros!(#[macro_export] { eprint, eprintln, dbg }, concat, |value: &str| {
+::custom_print::define_macros!(#[macro_export] { print, println, dbg }, concat, |value: &str| {
   unsafe {
-    crate::printf(format!("error: {value}\0").as_ptr());
+    crate::print::put_str_raw(value);
   }
 });
+
+::custom_print::define_macros!(#[macro_export] { eprint, eprintln }, concat, |value: &str| {
+  unsafe {
+    crate::print::put_str_raw(format!("error: {value}").as_str());
+  }
+});
+
+#[cfg(debug_assertions)]
+#[macro_export]
+macro_rules! debug {
+  ($( $args:expr ),*) => {
+    print!("[DEBUG] ");
+    println!( $( $args ),* );
+  }
+}
